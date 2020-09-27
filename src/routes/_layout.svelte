9075ed3f827856c09deefdc5cda3d31315f668ea
Eric Liu <ericyl.us@gmail.com>
2020-09-27 10:58:20 -0700

    Updates

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let segment = undefined;
+
+  import {
+    Content,
+    Breadcrumb,
+    BreadcrumbItem,
+    Grid,
+    Row,
+    Column,
+    Tabs,
+    TabContent,
+    Tab,
+    Select,
+    SelectItem,
+  } from "carbon-components-svelte";
+  import Header from "../components/Header.svelte";
+  import Theme from "../components/Theme.svelte";
+</script>
+
+<Theme persist theme="g10">
+  <Header segment="{segment}" />
+  <Content style="background: none; padding: 1rem">
+    <Grid>
+      <slot />
+    </Grid>
+  </Content>
+</Theme>
